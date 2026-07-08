1802a90ddde29ac8c3cfd0fc04bb8185b40399de
RuchirPatel007 <ruchat87@gmail.com>
2026-07-08 10:42:07 +0800

    Added auto token generation

--- mysite/mercury/apps.py
+++ mysite/mercury/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class MercuryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mercury'
+    def ready(self):
+        print("APP READY!")
+        import mercury.signals
