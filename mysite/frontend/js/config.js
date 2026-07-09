10ce3ef9072ed3eaaf6770a95df15f7edfb29849
RuchirPatel007 <ruchat87@gmail.com>
2026-07-09 20:05:47 +0800

    remove frontend

--- mysite/frontend/js/config.js
+++
@@ -1,7 +0,0 @@
-// mercury frontend config
-// Change API_BASE / SITE_BASE if your Django server runs somewhere else.
-const CONFIG = {
-  SITE_BASE: 'http://127.0.0.1:8000',
-  API_BASE: 'http://127.0.0.1:8000/api',
-  STRIPE_PUBLIC_KEY: 'pk_test_51Tr89xLjq8EzQHA8jbZ975R8ClIpzsAOnByqj4jsXKuA4teqHgSYmnBgi8aF7nTjEimdLBppfIMERdAG5s2fiXjx00DdsD9qoq',
-};
